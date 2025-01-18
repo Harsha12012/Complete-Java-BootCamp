5aabc8207affeebb8d47766999c4a2f32c0e183d
Harsha Deshmukh <harshadeshmukh12012@gmail.com>
2025-01-18 17:40:56 +0530

    new cal class created

---
+++ Core Java/01.Intro_To_Java/Cal.java
@@ -0,0 +1,5 @@
+public class Cal {
+    public void sum(int a,int b){
+        System.out.println("sum = "+(a+b));
+    }
+}
--- Core Java/01.Intro_To_Java/HelloWorld.java
+++ Core Java/01.Intro_To_Java/HelloWorld.java
@@ -2,5 +2,9 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("Bhupendra is very good boy ");
+
+        Cal obj=  new Cal();
+        obj.sum(10,20);
     }
 }
